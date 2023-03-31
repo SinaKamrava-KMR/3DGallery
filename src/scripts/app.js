714cffc9b444459a8e703933d89cd31e8f05fd95
Sina Kamrava <sinakamrava3118@gmail.com>
2023-03-31 19:41:50 +0330

    finished 3d shape

---
+++ src/scripts/app.js
@@ -0,0 +1,14 @@
+
+// let cards = document.querySelectorAll(".card");
+// let color;
+// setInterval(() => {
+//   color = randomColor()
+//   cards.forEach((card) => {
+//    card.style.backgroundColor=color
+//   })
+// }, 100);
+
+
+// function randomColor() {
+//   return `#${Math.trunc(Math.random()*143556).toString(16)}`
+// }
